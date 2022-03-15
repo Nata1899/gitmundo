26c4df7e42b361c5a957155fa08d67fbc7360bc5
091910333 Natanael <natanaelhernandezjuarez38@gmail.com>
2022-03-15 10:54:38 -0600

    SNAPSHOT 002 DEL PROYECTO, solo modificamos index

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title></title>
+</head>
+<body>
+
+	<font size="14" name="Tempus Sans TIC" color="Blue">
+		
+	<font size="14" name="Tempus Sans TIC" color="Blue">
+		<Marquee>
+
+			SALUDOS CORDIALES
+		</Marquee>
+	</font>
+</body>
+</html>
